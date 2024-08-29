a662900be54f3358fec9392fde1c9a32c6994d5d
Renzo Cabral <rnzcbrl@gmail.com>
2024-08-29 09:44:09 +0800

    Initial Commit

---
+++ Exercise: OOP Classes and Objects/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Exercise: OOP Classes and Objects
+//
+//  Created by Renzo Cabral on 8/29/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
